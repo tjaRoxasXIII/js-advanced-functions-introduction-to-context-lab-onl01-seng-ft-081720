6d47dd0f562fc5707874d9c9575413bbdb23595d
Taylor <tjaroguetech@gmail.com>
2020-11-27 13:53:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+let createEmployeeRecord = function(arr) {
+    return {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+let createEmployeeRecords = function(nest_arr) {
+    let records = []
+    for(const arr of nest_arr) {
+        records.push(createEmployeeRecord(arr))
+    }
+    return records
+}
+
+let createTimeInEvent = function(employeeRec, timeIn) {
+    let inObj = {
+        type: 'TimeIn',
+        hour: timeIn.split(' ')[1].parseInt(),
+        date: timeIn.split(' ')[0]
+    }
+    employeeRec.timeInEvents.push(inObj)
+    return employeeRec
+}
